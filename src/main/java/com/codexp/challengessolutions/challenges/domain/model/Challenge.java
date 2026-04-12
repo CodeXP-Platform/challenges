fd9d4dd0e75805297c02dc93c1c2351584a3790c
JonatanFD <u20211b995@upc.edu.pe>
2026-04-12 16:45:13 -0500

    feat: add challenge entity

---
+++ src/main/java/com/codexp/challengessolutions/challenges/domain/model/Challenge.java
@@ -0,0 +1,38 @@
+package com.codexp.challengessolutions.challenges.domain.model;
+
+import com.codexp.challengessolutions.challenges.domain.model.valueobjects.*;
+import com.codexp.challengessolutions.shared.domain.model.AbstractEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Challenge extends AbstractEntity {
+    @EmbeddedId
+    private ChallengeId id;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "title"))
+    private ChallengeTitle title;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "description"))
+    private ChallengeDescription description;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "authorId"))
+    private AuthorId authorId;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "difficulty"))
+    private ChallengeDifficulty difficulty;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "rewardPoints"))
+    private RewardPoints rewardPoints;
+
+    @Column(name = "isPublished")
+    private Boolean isPublished;
+}
