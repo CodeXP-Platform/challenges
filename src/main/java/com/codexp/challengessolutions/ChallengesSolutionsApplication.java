68f9d7b2308470e4c2f8edb760a6af5da5f1c6c6
JonatanFD <u20211b995@upc.edu.pe>
2026-04-11 19:39:29 -0500

    chore: first commit

---
+++ src/main/java/com/codexp/challengessolutions/ChallengesSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.codexp.challengessolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengesSolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengesSolutionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codexp/challengessolutions/ChallengesSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codexp.challengessolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengesSolutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
