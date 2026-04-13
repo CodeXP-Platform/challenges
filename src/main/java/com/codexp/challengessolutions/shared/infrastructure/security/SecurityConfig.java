04bb98bf3af44a446f30f685285e8a76a226cbf8
JonatanFD <u20211b995@upc.edu.pe>
2026-04-12 20:50:56 -0500

    feat: fix for app name

--- src/main/java/com/codexp/challengessolutions/shared/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/codexp/challengessolutions/shared/infrastructure/security/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/actuator/health").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
